7f90dce136e7d82ca043d3a64b621f0039beb238
Hussain Jawadwala <hjawadwala@zolute.co>
2024-10-17 15:56:20 +0530

    estructured

--- sdks/unifiapi/__init__.py
+++
@@ -1,4 +0,0 @@
-from sdks.unifiapi.api import controller, quiet, DEVICES, DPI, cat_app_to_dpi, dpi_to_cat_app, UnifiApiError
-
-#__all__ = [ "controller" ]
-
--- unifi.py
+++ unifi.py
@@ -1,4 +1,4 @@
-import sdks.unifiapi as us
-c = us.controller(endpoint='https://192.168.1.200:8443', username='', password='ubnt', verify=False)
+import unifiapi
+c = unifiapi.controller(endpoint='https://192.168.1.200:8443', username='', password='ubnt', verify=False)
 s = c.sites['default']()
 s.devices()
